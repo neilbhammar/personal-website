80c5565f3ffe613824d9ba88a66c4def21d25109
Neil Bhammar <nbhammar2000@gmail.com>
2025-03-10 21:49:15 -0400

    Update build configuration for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -30,7 +30,12 @@ export default defineConfig({
   },
   root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
+    outDir: "dist",
     emptyOutDir: true,
+    sourcemap: true
   },
+  server: {
+    port: 3000,
+    host: true
+  }
 });
