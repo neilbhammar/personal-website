c679f1854841b76b8c97f9a3dadfb185afd2cb9c
Neil Bhammar <nbhammar2000@gmail.com>
2025-03-13 15:10:48 -0400

    Small change to vite.config.ts to make sure this can also run locally

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { fileURLToPath } from 'url';
 
-// Determine the project root
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Now you can use __dirname as before
 const projectRoot = path.resolve(__dirname);
 const clientRoot = path.resolve(projectRoot, "client");
 
